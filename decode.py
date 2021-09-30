2441b101e31a661950bb348028ff5b6a06ed1799
aidandegooyer <acdegooy@syr.edu>
2021-09-30 18:09:03 -0400

    decode should work, added input file

--- decode.py
+++ decode.py
@@ -1,1 +1,29 @@
 # decode python program
+
+import csv
+
+reader = csv.reader(open('table.csv', 'r'))
+char_dict = {}
+for row in reader:
+    k, v = row
+    char_dict[k] = v.replace('"', '')
+
+infile = open(input('What is the name of the file?'))
+instr = infile.read()
+outstr = ''
+
+x = 0
+
+for i in range(len(instr)):
+    if x >= len(instr):
+        break
+    if instr[x] == '1':
+        current_word = instr[x:x+4]
+        x += 4
+    else:
+        current_word = instr[x:x+7]
+        x += 7
+    current_char = char_dict[current_word]
+    outstr += current_char
+
+print(outstr)
